57c09430c23b7042ccbf22c6a9a1587c80a7f146
ashishpaudel123 <ashishpaudel206058@gmail.com>
2026-01-11 09:50:34 +0545

    fix: initialized AOS for animations

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-createRoot(document.getElementById('root')!).render(
+AOS.init({
+  duration: 800,
+  easing: "ease-out-cubic",
+  once: false,
+  offset: 50,
+  delay: 0,
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
